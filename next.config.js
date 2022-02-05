e243af0d4bcd46adc1bd41c74303debd22981ac2
Landon Johnson <59711105+ljtechdotca@users.noreply.github.com>
2022-02-04 23:27:12 -0600

    [fix] public runtime config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  publicRuntimeConfig: {
+    url:
+      process.env.NODE_ENV === "development"
+        ? "http://localhost:3000"
+        : "https://ljtech-sign-in-providers.vercel.app",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,18 @@
 import { Auth0Provider } from "@auth0/auth0-react";
 import type { AppProps } from "next/app";
+import getConfig from "next/config";
 import "../styles/globals.scss";
+
+const {
+  publicRuntimeConfig: { url },
+} = getConfig();
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Auth0Provider
       domain="dev-l-l2xrbs.us.auth0.com"
       clientId="dh3VzgHVOqYALi1os1FZBqkzOCn3s5Fl"
-      redirectUri="https://ljtech-sign-in-providers.vercel.app"
+      redirectUri={url}
     >
       <Component {...pageProps} />
     </Auth0Provider>
