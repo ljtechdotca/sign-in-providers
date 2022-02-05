64abc512a32f8373849573ac242cd3ec9162bbf3
Landon Johnson <59711105+ljtechdotca@users.noreply.github.com>
2022-02-04 23:16:12 -0600

    [feat] auth0 tests

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,16 @@
-import { SessionProvider } from "next-auth/react";
+import { Auth0Provider } from "@auth0/auth0-react";
 import type { AppProps } from "next/app";
 import "../styles/globals.scss";
 
-function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <SessionProvider session={session}>
+    <Auth0Provider
+      domain="dev-l-l2xrbs.us.auth0.com"
+      clientId="dh3VzgHVOqYALi1os1FZBqkzOCn3s5Fl"
+      redirectUri="http://localhost:3000"
+    >
       <Component {...pageProps} />
-    </SessionProvider>
+    </Auth0Provider>
   );
 }
 
